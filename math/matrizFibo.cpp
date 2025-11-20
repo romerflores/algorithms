12b5e1d1037e25ca9ce4d3fc7abcdb6419d6e0fe
Romer <107067537+romerflores@users.noreply.github.com>
2025-11-19 20:24:26 -0400

    Actualizacion de fibo

--- math/matrizFibo.cpp
+++ math/matrizFibo.cpp
@@ -1,35 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll MOD = 1'000'000'007;
+
 struct Matrix {
-    long long mat[2][2];
-    Matrix friend operator *(const Matrix &a, const Matrix &b){
-        Matrix c;
-        for (int i = 0; i < 2; i++) {
-          for (int j = 0; j < 2; j++) {
-              c.mat[i][j] = 0;
-              for (int k = 0; k < 2; k++) {
-                  c.mat[i][j] += a.mat[i][k] * b.mat[k][j];
-              }
-          }
-        }
-        return c;
-    }
+    ll mat[2][2];
 };
-Matrix matpow(Matrix base, long long n) {
-    Matrix ans{ {
-      {1, 0},
-      {0, 1}
-    } };
+
+Matrix operator*(const Matrix &a, const Matrix &b){
+    Matrix c{};
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++) {
+            __int128 tmp = 0;
+            for (int k = 0; k < 2; k++)
+                tmp += (__int128)a.mat[i][k] * b.mat[k][j];
+            c.mat[i][j] = (ll)(tmp % MOD);
+        }
+    return c;
+}
+
+Matrix matpow(Matrix base, ll n) {
+    Matrix ans{{ {1, 0}, {0, 1} }};
     while (n) {
-        if(n&1)
-            ans = ans*base;
-        base = base*base;
+        if (n & 1) ans = ans * base;
+        base = base * base;
         n >>= 1;
     }
     return ans;
 }
-ll fib(int n) {
-    Matrix base{ {
-      {1, 1},
-      {1, 0}
-    } };
+
+ll fib(ll n) {
+    if (n == 0) return 0;
+    Matrix base{{ {1,1}, {1,0} }};
     return matpow(base, n).mat[0][1];
+}
+
+int main(){
+    ll n;
+    cin >> n;
+    cout << fib(n) << "\n";
 }
