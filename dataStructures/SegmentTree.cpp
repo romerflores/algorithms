afbb04fab6c7b842d09216870ab65b70fd33841d
romerflores <romer141016@gmail.com>
2026-05-20 12:56:18 -0400

    add segment Tree

--- dataStructures/SegmentTree.cpp
+++ dataStructures/SegmentTree.cpp
@@ -1,70 +1,60 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-typedef long long ll;
-
-struct Data
+struct Node
 {
-    ll cant = 0;
-    Data() { cant = 1e18; }
-
-    Data(ll c) { cant = c; }
+    //constructor
+    Node operator+(const Node &other)const
+    {
+        //merge
+    }
 };
 
-struct SegTree
+struct SegmentTree
 {
-private:
-    vector<Data> st;
-
-public:
     int sz;
-    Data merge(Data a, Data b)
+    vector<Node> st;
+ 
+    SegmentTree(int n,vector<ll> &v)
     {
-        return min(a.cant, b.cant);
-    }
-    void init(int n, vector<Data> v)
-    {
-        while (__builtin_popcount(n) != 1)
+        if(__builtin_popcount(n)!=1)n=(1<<(31-__builtin_clz(n)+1));
+        sz=n;
+        st.assign(2*sz,Node());
+        for(int i=0;i<(int)v.size();i++)
         {
-            n++;
+            st[sz+i]=Node(v[i]);
         }
-        st.resize(2 * n, Data());
-        sz = n; // solo n, NO 2*n
-        for (int i = 0; i < (int)v.size(); i++)
+        for(int i=2*sz-1;i>=2;i-=2)
         {
-            st[n + i] = v[i];
-        }
-
-        for (int i = n - 1; i > 0; --i)
-        {
-            st[i] = merge(st[i << 1], st[(i << 1) + 1]);
+            int p=i/2;
+            st[p]=st[p*2]+st[p*2+1];//orden importanteeeeeee aaaaa media hora debuguenado mierda aaaaaaasex0000
         }
     }
-
-    void updateTreeNode(int p, Data nuevoValor) // 0i pos
+ 
+    void update(ll num,int pos)//0i
     {
-        st[p + sz] = nuevoValor;
-        p = p + sz;
-        for (int i = p; i > 1; i >>= 1)
-            st[i >> 1] = merge(st[i], st[i ^ 1]);
+        st[sz+pos]=Node(num);
+        pos+=sz;
+        while(pos>1)
+        {
+            int par=pos/2;
+            st[par]=st[2*par]+st[2*par+1];
+            pos=par;
+        }
     }
-
-    Data query(int nodo, int left_nodo, int right_nodo, int l_q, int r_q) //
+ 
+    Node query(int nodo,int l_n,int r_n,int l_q,int r_q)
     {
-        // query(1,0,st.sz-1,l_q,r_q) tipo->[l_q,r_q]0i
-        /*
-         los indices de los nodos empieza desde 1;
-         */
-        if (l_q <= left_nodo && right_nodo <= r_q)
-        {
-            return st[nodo];
-        }
-        if (l_q > right_nodo || left_nodo > r_q)
-        {
-            return Data();
-        }
-
-        int mitad = (left_nodo + right_nodo) / 2; ///[l:r] --> [l:mitad] [mitad+1:r]
-        return merge(query(nodo * 2, left_nodo, mitad, l_q, r_q), query(nodo * 2 + 1, mitad + 1, right_nodo, l_q, r_q));
+        //si tengo todo retorna todo
+        if(l_q<=l_n and r_n<=r_q)return st[nodo];
+        // si no tengo nada retorna neutro
+        if(l_q>r_n or l_n>r_q)return Node();//neutro
+        int m=(l_n+r_n)/2;
+        return query(2*nodo,l_n,m,l_q,r_q)+query(2*nodo+1,m+1,r_n,l_q,r_q);
+    }
+ 
+    int first_ge(int nodo,int l_n,int r_n,ll num)
+    {
+        if(l_n==r_n)return l_n;
+        int m=(l_n+r_n)/2;
+        if(st[2*nodo].maxi>=num)return first_ge(nodo*2,l_n,m,num);
+        else return first_ge(nodo*2+1,m+1,r_n,num);
     }
 };
